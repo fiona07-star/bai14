f35493749304eb7a31e4130ea362b0921aa26098
fiona07-star <quynhnnk24406h2@st.uel.edu.vn>
2026-09-21 14:45:37 +0700

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Catalog } from './catalog/catalog'; // 1. Import Catalog component
+import { Catalog } from './catalog/catalog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Catalog], // 2. Thêm Catalog vào mảng imports ở đây
+  imports: [RouterOutlet, Catalog],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
